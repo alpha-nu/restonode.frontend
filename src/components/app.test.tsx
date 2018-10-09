9376f013d7827da14ed9336c06a04ced66195b59
AK <karkoukli.anas@gmail.com>
2018-10-08 23:07:51 -0300

    refactor tests

--- src/components/app.test.tsx
+++ src/components/app.test.tsx
@@ -17,45 +17,38 @@ const user: IUser = {
     logins: []
 };
 
+const app = <App user={user}
+    addMeal={jest.fn()}
+    checkout={jest.fn()}
+    loggedInUser={user.current}
+    switchLogin={jest.fn()}
+    restaurants={{ all: [], selected: { address: '', id: 0, name: '', rating: '' } }}
+    fetchRestaurants={jest.fn()}
+    selectRestaurant={jest.fn()}
+    fetchMeals={jest.fn()}
+    selectedRestaurant={{ name: '', id: 0, address: '', rating: '' }} />;
+
 test('<App /> renders its child components and defaults to restaurant view', () => {
 
-    const app = mount(
+    const mountedApp = mount(
         <MemoryRouter initialEntries={['/restaurants', '/restaurants/1']} initialIndex={0}>
-            <App user={user}
-                addMeal={jest.fn()}
-                checkout={jest.fn()}
-                loggedInUser={user.current}
-                switchLogin={jest.fn()}
-                restaurants={{ all: [], selected: { address: '', id: 0, name: '', rating: '' } }}
-                fetchRestaurants={jest.fn()}
-                selectRestaurant={jest.fn()}
-                fetchMeals={jest.fn()}
-                selectedRestaurant={{ name: '', id: 0, address: '', rating: '' }} />
+            {app}
         </MemoryRouter>
     );
-    expect(app.find(Navigation).exists()).toBe(true);
-    expect(app.find(Order).exists()).toBe(true);
-    expect(app.find(Restaurants).exists()).toBe(true);
-    expect(app.find(Menu).exists()).toBe(false);
+    expect(mountedApp.find(Navigation).exists()).toBe(true);
+    expect(mountedApp.find(Order).exists()).toBe(true);
+    expect(mountedApp.find(Restaurants).exists()).toBe(true);
+    expect(mountedApp.find(Menu).exists()).toBe(false);
 });
 
 test('<App /> renders menu view for a selected restaurant', () => {
-    const app = mount(
+    const mountedApp = mount(
         <MemoryRouter initialEntries={['/restaurants', '/restaurants/1']} initialIndex={1}>
-            <App user={user}
-                addMeal={jest.fn()}
-                checkout={jest.fn()}
-                loggedInUser={user.current}
-                switchLogin={jest.fn()}
-                restaurants={{ all: [], selected: { address: '', id: 0, name: '', rating: '' } }}
-                fetchRestaurants={jest.fn()}
-                selectRestaurant={jest.fn()}
-                fetchMeals={jest.fn()}
-                selectedRestaurant={{ name: '', id: 0, address: '', rating: '' }} />
+            {app}
         </MemoryRouter>
     );
-    expect(app.find(Navigation).exists()).toBe(true);
-    expect(app.find(Order).exists()).toBe(true);
-    expect(app.find(Restaurants).exists()).toBe(false);
-    expect(app.find(Menu).exists()).toBe(true);
+    expect(mountedApp.find(Navigation).exists()).toBe(true);
+    expect(mountedApp.find(Order).exists()).toBe(true);
+    expect(mountedApp.find(Restaurants).exists()).toBe(false);
+    expect(mountedApp.find(Menu).exists()).toBe(true);
 });
