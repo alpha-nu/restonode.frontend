6cd5e0b6a6064a8ffdfb5b291a133b3565998596
AK <karkoukli.anas@gmail.com>
2018-10-10 22:08:27 -0300

    nock to mock api requests in action creators

--- src/actions/meal.test.ts
+++ src/actions/meal.test.ts
@@ -1,8 +1,18 @@
 import { fetchMeals, FETCH_MEALS_ERROR } from './meal';
+import * as nock from 'nock';
+import { apiEndPoint } from '../config/endpoints';
+
+beforeAll(() => {
+    nock.disableNetConnect();
+});
 
 test('dispatches error action in case of fetch errors', async () => {
+    nock(apiEndPoint)
+        .get('/v1/order-management/restaurants/500/meals')
+        .reply(500, 'error');
+
     const dispatcher = jest.fn();
-    await fetchMeals(1)(dispatcher);
+    await fetchMeals(500)(dispatcher);
 
     expect(dispatcher.mock.calls[1][0]).toEqual({ type: FETCH_MEALS_ERROR });
 });
--- src/actions/restaurants.test.ts
+++ src/actions/restaurants.test.ts
@@ -1,6 +1,14 @@
 import { fetchRestaurants, FETCH_RESTAURANTS_ERROR } from './restaurant';
+import * as nock from 'nock';
+import { apiEndPoint } from '../config/endpoints';
+
+beforeAll(() => {
+    nock.disableNetConnect();
+});
 
 test('dispatches error action in case of fetch errors', async () => {
+    nock(apiEndPoint).get('/v1/order-management/restaurants').reply(500, 'error');
+
     const dispatcher = jest.fn();
     await fetchRestaurants()(dispatcher);
 
