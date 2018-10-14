699535f31f71b21f5a762546eba274277cde7fd0
AK <karkoukli.anas@gmail.com>
2018-10-14 19:16:51 -0300

    nitpick... made it a tail recursive call

--- src/actions/restaurant.ts
+++ src/actions/restaurant.ts
@@ -120,13 +120,14 @@ export const newRestaurant = (restaurant: ICreateRestaurantAttributes) => {
 
 const mapErrors = (error: any) => {
     const recurseErrors = (err: any): any => {
+        if (err.children.length === 0) {
+            const constraints = err.constraints;
+            return Object.keys(constraints).map(_ => constraints[_]).join(', ');
+        }
 
         for (const child of err.children) {
             return recurseErrors(child);
         }
-
-        const constraints = err.constraints;
-        return Object.keys(constraints).map(_ => constraints[_]).join(', ');
     };
 
     return error.message.reduce((acc: any, _: any) => {
